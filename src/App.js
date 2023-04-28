a70f7bf831d4810f695ad4405e2b5bd82edeb760
Tomomi-K1 <tomomi.music@gmail.com>
2023-04-27 17:38:43 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import './App.css';
+import React from 'react';
+// import Clicker from './Clicker';
+// import ButtonGroup from './ButtonGroup';
+// import Counter from './Counter';
+import NumberGame from './NumberGame';
+
+function App() {
+  return (
+    <div className='App'>
+      <NumberGame />
+      {/* <ButtonGrousp /> */}
+      {/* <Counter /> */}
+      {/* <Clicker />
+      <Clicker />
+      <Clicker /> */}
+    </div>
+  );
+}
+
+export default App;
